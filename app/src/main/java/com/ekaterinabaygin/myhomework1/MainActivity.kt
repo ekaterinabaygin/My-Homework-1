04df2a8c56704d42a7142bae0d5ef987ebd86e30
Baygin_Ekaterina <85458383+ekaterinabaygin@users.noreply.github.com>
2024-01-11 18:10:46 +0300

    MainActivity edition

--- app/src/main/java/com/ekaterinabaygin/myhomework1/MainActivity.kt
+++ app/src/main/java/com/ekaterinabaygin/myhomework1/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.ekaterinabaygin.myhomework1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun main() {
-        val car = Transport.Car("Toyota", "Camry")
-        val bicycle = Transport.Bicycle("Mountain")
-
-        describeTransport(car)
-        describeTransport(bicycle)
-    }
 }
+
+val car = Transport.Car("Toyota", "Camry")
+val bicycle = Transport.Bicycle("Mountain")
+
+
